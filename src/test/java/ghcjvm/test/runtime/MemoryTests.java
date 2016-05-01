513fae4c7f4163f192b2f92c2c0c6c72adaa55df
Thomas Jakway <tjakway@nyu.edu>
2016-05-01 02:26:09 -0400

    added test stub class

---
+++ src/test/java/ghcjvm/test/runtime/MemoryTests.java
@@ -0,0 +1,21 @@
+package ghcjvm.runtime.tests.MemoryTests
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MemoryTests
+{
+    @Before
+    public void setUp()
+    {
+
+    }
+
+    @After
+    public void tearDown()
+    {
+
+    }
+
+
+}
