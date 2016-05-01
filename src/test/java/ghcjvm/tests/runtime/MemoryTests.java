321bd157996cb88cb1226d61854cca7534f9b44a
Thomas Jakway <tjakway@nyu.edu>
2016-05-01 02:49:22 -0400

    wrote singleton sanity check

--- src/test/java/ghcjvm/tests/runtime/MemoryTests.java
+++ src/test/java/ghcjvm/tests/runtime/MemoryTests.java
@@ -1,14 +1,18 @@
-package ghcjvm.runtime.tests;
+package ghcjvm.tests.runtime;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import lljvm.runtime.Memory;
+
 public class MemoryTests
 {
+    private Memory mem;  
+
     @Before
     public void setUp()
     {
-
+        mem = Memory.getMemorySingleton();
     }
 
     @After
@@ -18,4 +22,15 @@ public class MemoryTests
     }
 
 
+    /**
+     * sanity check that the singleton method works
+     */
+    @Test
+    public void testGetSingleton()
+    {
+        final Memory m = Memory.getMemorySingleton();
+        assertNotNull(m);
+        
+        assertTrue(m instanceof Memory);
+    }
 }
